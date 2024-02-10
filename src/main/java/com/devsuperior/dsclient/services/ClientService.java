c01a902277b662387ef7029b9e61cbe484054e1e
Vinicius Parente <vd.parente@hotmail.com>
2024-02-10 17:31:59 -0300

    Project Finish

---
+++ src/main/java/com/devsuperior/dsclient/services/ClientService.java
@@ -0,0 +1,83 @@
+package com.devsuperior.dsclient.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dsclient.dto.ClientDTO;
+import com.devsuperior.dsclient.entities.Client;
+import com.devsuperior.dsclient.repositories.ClientRepository;
+import com.devsuperior.dsclient.services.exceptions.DatabaseException;
+import com.devsuperior.dsclient.services.exceptions.ResourceNotFoundException;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@Service
+public class ClientService {
+    
+    @Autowired
+    private ClientRepository repository;
+    
+    @Transactional(readOnly = true)
+ 	public Page<ClientDTO> findAllPaged(PageRequest pageRequest ){
+		Page<Client>  list = repository.findAll(pageRequest);
+
+		return list.map(x-> new ClientDTO(x));
+    }
+
+    @Transactional(readOnly = true)
+    public ClientDTO findById(Long id) {
+        Optional<Client> obj = repository.findById(id);
+        Client entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found!"));
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+	public ClientDTO insert(ClientDTO dto) {
+		Client entity = new Client();
+		copyDtoToEntity(dto, entity);
+		entity = repository.save(entity);
+		return new ClientDTO(entity);
+	}
+
+    @Transactional
+	public ClientDTO update(Long id, ClientDTO dto) {
+		try {
+			Client entity = repository.getReferenceById(id);
+			copyDtoToEntity(dto, entity);
+			entity = repository.save(entity);
+			return new ClientDTO(entity);
+		}
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id Not Found "+ id);
+		}
+
+	}
+
+
+	public void delete(Long id) {
+		try {
+			repository.deleteById(id);
+		}
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id Not Found "+ id);
+		}
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
+		entity.setName(dto.getName());
+		entity.setCpf(dto.getCpf());
+		entity.setIncome(dto.getIncome());
+		entity.setBirthDate(dto.getBirthDate());
+		entity.setChildren(dto.getChildren());
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,40 @@
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Joaquim Pinto', '02344510862', 3000, TIMESTAMP WITH TIME ZONE '1983-07-25T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Orlando Morando', '12344710862', 3500, TIMESTAMP WITH TIME ZONE '1963-02-15T10:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Jenésio de Oliveira', '02264510762', 4000, TIMESTAMP WITH TIME ZONE '1973-05-05T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Carlos de Oliveira', '13264510962', 2000, TIMESTAMP WITH TIME ZONE '1994-10-15T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Eduardo Junior', '13364510762', 1500, TIMESTAMP WITH TIME ZONE '1953-11-16T10:30:00Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Odair Jose', '02244510762', 1600, TIMESTAMP WITH TIME ZONE '1995-07-23T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Evandro Mesquita', '44464510762', 2300, TIMESTAMP WITH TIME ZONE '1976-03-19T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Sandra de Oliveira', '02267810762', 1000, TIMESTAMP WITH TIME ZONE '1963-04-25T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Valdir de Carvalho', '02264330762', 2500, TIMESTAMP WITH TIME ZONE '1993-08-05T10:30:00Z', 5);  
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Carlos Ferreira', '01264330762', 3500, TIMESTAMP WITH TIME ZONE '1994-06-05T10:30:00Z', 0); 
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Rogério Dantas', '12344510862', 3100, TIMESTAMP WITH TIME ZONE '1983-09-25T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Simone Nonato', '13344710862', 2500, TIMESTAMP WITH TIME ZONE '1963-02-15T10:30:00Z', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Cassio Rodrigues', '22264510762', 3000, TIMESTAMP WITH TIME ZONE '1973-12-05T10:30:00Z', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Eustaquio Pereira', '43264510962', 2500, TIMESTAMP WITH TIME ZONE '1994-11-15T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Genésio de Arruda', '14364510762', 1800, TIMESTAMP WITH TIME ZONE '1953-12-16T10:30:00Z', 5);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Elder da Silva', '02254510762', 2500, TIMESTAMP WITH TIME ZONE '1995-03-23T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Mariana Dantas', '44564510762', 2900, TIMESTAMP WITH TIME ZONE '1976-02-19T10:30:00Z', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Marina Oliveira', '02298810762', 1900, TIMESTAMP WITH TIME ZONE '1963-01-25T10:30:00Z', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Rita Rodrigues', '02744330762', 2700, TIMESTAMP WITH TIME ZONE '1993-06-05T10:30:00Z', 5);  
+INSERT INTO tb_client (name, cpf, income, birth_date, children) 
+                 VALUES ('Maria de Oliveira', '01564330762', 3400, TIMESTAMP WITH TIME ZONE '1994-08-05T10:30:00Z', 0);  
